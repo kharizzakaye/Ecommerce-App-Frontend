826f1e7f88acf28e4dacae3576c9cec832e76e5f
Khariza <kharizzakaye@gmail.com>
2024-03-31 03:36:25 +0800

    Cart Button

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,18 +1,31 @@
-import { Text, StyleSheet } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import styles from "./Home.styles";
+import { Ionicons, Fontisto } from "@expo/vector-icons"
 
 const Home = () => {
     return (
         <SafeAreaView>
-            <Text>Home</Text>
+            <View style={styles.appBarWrapper}>
+                <View style={styles.appBar}>
+                    <Ionicons name="location-outline" size={24} />
+                    <Text style={styles.location}>Tokyo, Japan</Text>
+
+                    <View style={{ alignItems: "flex-end" }}>
+                        <View style={styles.cartCount}>
+                            <Text style={styles.cartNumber}>12</Text>
+                        </View>
+
+                        <TouchableOpacity>
+                            <Fontisto name="shopping-bag" size={24} />
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
 
 export default Home;
-
-
-const styles = StyleSheet.create({
-    
-})
